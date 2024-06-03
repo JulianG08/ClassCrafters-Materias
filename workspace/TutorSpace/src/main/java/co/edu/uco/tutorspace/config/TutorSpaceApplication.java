13126179bf0ec965f38ec2c2ad37b01fef245da8
Julian David Guarnizo Castrillon <89322945+JulianG08@users.noreply.github.com>
2024-06-03 16:53:21 -0500

    Generando el proyecto en Eclipse

---
+++ workspace/TutorSpace/src/main/java/co/edu/uco/tutorspace/config/TutorSpaceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.tutorspace.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorSpaceApplication.class, args);
+	}
+
+}
---
+++ workspace/TutorSpace/src/test/java/co/edu/uco/tutorspace/config/TutorSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.tutorspace.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
