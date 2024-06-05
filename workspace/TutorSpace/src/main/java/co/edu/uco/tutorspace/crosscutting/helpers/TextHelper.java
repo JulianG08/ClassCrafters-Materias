44856b51215cc8b207b19adbd64188248f109b3b
Julian David Guarnizo Castrillon <89322945+JulianG08@users.noreply.github.com>
2024-06-04 23:57:49 -0500

    Generación de helpers y comienzo DTO's

---
+++ workspace/TutorSpace/src/main/java/co/edu/uco/tutorspace/crosscutting/helpers/TextHelper.java
@@ -0,0 +1,30 @@
+package co.edu.uco.tutorspace.crosscutting.helpers;
+
+public final class TextHelper {
+
+	public static final String EMPTY = "";
+	
+	private TextHelper() {
+		super();
+	}
+	
+	public static final boolean isNull(final String string) {
+		return ObjectHelper.getObjectHelper().isNull(string);
+	}
+	
+	public static final boolean isNullOrEmpty(final String string) {
+		return isNull(string) || EMPTY.equals(applyTrim(string));
+	}
+	
+	public static final String applyTrim(final String string) {
+		return getDefault(string).trim();
+	}
+	
+	public static final String getDefault(final String string, final String defaultValue) {
+		return ObjectHelper.getObjectHelper().getDefault(string, defaultValue);
+	}
+	
+	public static final String getDefault(final String string) {
+		return getDefault(string, EMPTY);
+	}
+}
---
+++ workspace/TutorSpace/src/main/java/co/edu/uco/tutorspace/dto/MateriaDTO.java
@@ -0,0 +1,7 @@
+package co.edu.uco.tutorspace.dto;
+
+public final class MateriaDTO {
+
+	private int id;
+	private String nombre;
+}
