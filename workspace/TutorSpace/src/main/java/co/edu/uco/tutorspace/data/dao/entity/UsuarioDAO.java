1a6454a4888c9cb1fe8a73dbc022ae66fc7fa343
KevinMurcia636 <kevinmurciamo636@gmail.com>
2024-06-09 17:06:43 -0500

    userdao

---
+++ workspace/TutorSpace/src/main/java/co/edu/uco/tutorspace/data/dao/entity/UsuarioDAO.java
@@ -0,0 +1,6 @@
+package co.edu.uco.tutorspace.data.dao.entity;
+
+import co.edu.uco.tutorspace.entity.UsuarioEntity;
+
+public interface UsuarioDAO extends CrearDAO<UsuarioEntity>, ActualizarDAO<UsuarioEntity>, ConsultarDAO<UsuarioEntity>, EliminarDAO<UsuarioEntity> {
+}
