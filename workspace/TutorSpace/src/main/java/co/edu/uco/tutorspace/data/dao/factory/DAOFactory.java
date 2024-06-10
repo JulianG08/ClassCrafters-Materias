a855d2f3ee8301047bebb18265c95278dddb3b67
Julian David Guarnizo Castrillon <89322945+JulianG08@users.noreply.github.com>
2024-06-09 21:44:20 -0500

    Corrigiendo error de nombramiento

--- workspace/TutorSpace/src/main/java/co/edu/uco/tutorspace/data/dao/factory/DAOFactory.java
+++ workspace/TutorSpace/src/main/java/co/edu/uco/tutorspace/data/dao/factory/DAOFactory.java
@@ -2,11 +2,11 @@ package co.edu.uco.tutorspace.data.dao.factory;
 
 import co.edu.uco.tutorspace.data.dao.entity.UsuarioDAO;
 import co.edu.uco.tutorspace.data.dao.entity.MateriaDAO;
-import co.edu.uco.tutorspace.data.dao.factory.concrete.PostgreSQLDAOFactory;
+import co.edu.uco.tutorspace.data.dao.factory.concrete.PostgreSqlDAOFactory;
 
 public interface DAOFactory {
     static DAOFactory getFactory() {
-        return new PostgreSQLDAOFactory();
+        return new PostgreSqlDAOFactory();
     }
 
     void cerrarConexion();
