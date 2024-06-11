57b7c1905a0f7586d37d42fd0cdf4e5619a4e853
Julian David Guarnizo Castrillon <89322945+JulianG08@users.noreply.github.com>
2024-06-10 22:13:32 -0500

    Update MessageCatalogStrategy.java

--- workspace/TutorSpace/src/main/java/co/edu/uco/tutorspace/crosscutting/exceptions/messagecatalog/MessageCatalogStrategy.java
+++ workspace/TutorSpace/src/main/java/co/edu/uco/tutorspace/crosscutting/exceptions/messagecatalog/MessageCatalogStrategy.java
@@ -7,39 +7,38 @@ import co.edu.uco.tutorspace.crosscutting.exceptions.messagecatalog.impl.Message
 import co.edu.uco.tutorspace.crosscutting.exceptions.messagecatalog.impl.MessageCatalogExternalService;
 import co.edu.uco.tutorspace.crosscutting.helpers.ObjectHelper;
 
-public class MessageCatalogStrategy {
+public final class MessageCatalogStrategy {
 
-	private static final MessageCatalog base= new MessageCatalogBase();
-	private static final MessageCatalog externalService= new MessageCatalogExternalService();
-	
-	static {
-		inicializar();
-	}
-	
-	private MessageCatalogStrategy() {
-		super();
-	}
-	
-	public static void inicializar() {
-		base.inicializar();
-		externalService.inicializar();
-	}
-	
-	private static final MessageCatalog getStrategy(final boolean isBase) {
-		return isBase ? base : externalService;
-	}
-	
-	public static final Mensaje getMensaje(final CodigoMensaje codigo, final String...parametros) {
-		if (ObjectHelper.getObjectHelper().isNull(codigo)) {
-			var mensajeUsuario = getContenidoMensaje(CodigoMensaje.M00002);
-			var mensajeTecnico = getContenidoMensaje(CodigoMensaje.M00001);
-			throw new CrossCuttingTutorSpaceException(mensajeTecnico, mensajeUsuario);
-		}
-		
-		return getStrategy(codigo.isBase()).obtenerMensaje(codigo, parametros);
-	}
+	private static final MessageCatalog base = new MessageCatalogBase();
+    private static final MessageCatalog externalService = new MessageCatalogExternalService();
 
-	public static final String getContenidoMensaje(final CodigoMensaje codigo, final String...parametros) {
-		return getMensaje(codigo, parametros).getContendio();		
-	}
+    static {
+        inicializar();
+    }
+
+    private MessageCatalogStrategy() {
+        super();
+    }
+
+    public static void inicializar() {
+        base.inicializar();
+        externalService.inicializar();
+    }
+
+    private static final MessageCatalog getStrategy(final boolean isBase) {
+        return isBase ? base: externalService;
+    }
+
+    public static final Mensaje getMensaje(final CodigoMensaje codigo, final String...parametros){
+        if (ObjectHelper.getObjectHelper().isNull(codigo)) {
+            var mensajeUsuario = getContenidoMensaje(CodigoMensaje.M00002);
+            var mensajeTecnico = getContenidoMensaje(CodigoMensaje.M00001);
+            throw new CrossCuttingTutorSpaceException(mensajeTecnico, mensajeUsuario);
+        }
+        return getStrategy(codigo.isBase()).obtenerMensaje(codigo, parametros);
+    }
+
+    public static final String getContenidoMensaje(final CodigoMensaje codigo, final String...parametros) {
+        return getMensaje(codigo, parametros).getContenido();
+    }
 }
